6ecdda212ba466da018bf4c2a06c196e89787657
Pedro Diaz <peterpunk@MacBook-Pro-de-Pedro.local>
2025-03-05 10:21:49 -0600

    fix: getUserById service fixed

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,22 +3,10 @@ import { IResponse } from "@/utils/types";
 
 export const getUserById = async (id: number) => {
   try {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
-    const response = await fetch(`${baseUrl}/api/user/getById`, {
-      method: "POST",
-      body: JSON.stringify({ id }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (!response.ok) {
-      throw new Error("Failed to get user information");
-    }
-    if (response.ok) {
-      return response.json();
-    }
-  } catch (error) {
-    console.error("Error fetching user information", error);
+    const response = await http.get<IResponse>(`/api/users/${id}`);
+    return response.data;
+  } catch (error: any) {
+    return error;
   }
 };
 
--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,13 +1,13 @@
 export interface UserType {
-    id: string,
-    username: string,
-    name?: string,
-    paternal_last_name?: string,
-    maternal_last_name?: string,
-    role_id: string,
-    role_display_name: string,
-    active: string,
-    active_display_name: string,
-    created_at: string,
-    updated_at: string
+  id: string;
+  username: string;
+  name?: string;
+  paternal_last_name?: string;
+  maternal_last_name?: string;
+  role_id: string;
+  role_display_name: string;
+  active: string;
+  active_display_name: string;
+  created_at: string;
+  updated_at: string;
 }
