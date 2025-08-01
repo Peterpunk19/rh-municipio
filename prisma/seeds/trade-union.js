d11b780490329d685fce91382b3488bee3c15d92
Pedro Diaz <peterpunk@rosa.tuxtla.local>
2025-08-01 13:26:21 -0600

    feat: trade union seed

--- prisma/seeds/trade-union.js
+++ prisma/seeds/trade-union.js
@@ -1,3 +1,11 @@
-const tradeUnion = [{ id: 1, name: "sindicato", display_name: "Sindicato" }];
+const tradeUnion = [
+  { id: 1, name: "seat", display_name: "SEAT" },
+  { id: 2, name: "sutesa", display_name: "SUTESA" },
+  { id: 3, name: "sutra", display_name: "SUTRA" },
+  { id: 4, name: "sutsum", display_name: "SUTSUM" },
+  { id: 5, name: "stamt", display_name: "STAMT" },
+  { id: 6, name: "stusat", display_name: "STUSAT" },
+  { id: 7, name: "strachisa", display_name: "STRACHISA" },
+];
 
 module.exports = tradeUnion;
